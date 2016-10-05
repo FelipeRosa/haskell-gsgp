5b56183392ec31938a152aa111e074d8376c2412
Felipe Soares Gonçalves Rosa <felipe.sgrosa@gmail.com>
2016-10-05 20:15:45 -0300

    added basic dataset module

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import GSGP.Data
+
 main :: IO ()
-main = print "oi"
+main = do
+  ds <- loadTxt "/Users/felipe/Desktop/housing.data.txt" :: IO (Dataset Double)
+
+  let trainingInput  = reshape ds (0, 13, 0, 300)
+      trainingOutput = reshape ds (13, 14, 0, 300)
+
+  print $ shape trainingInput
+  print $ shape trainingOutput
+
+  print $ elementAt trainingInput (0, 0)
+  print $ elementAt trainingOutput (0, 0)
